5af353edb0ec7892d6f295a4e056c1b62b2a847f
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-05-24 19:47:28 -0600

    Connect the Home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    //... other configurations
+    images: {
+      domains: ['images.pexels.com', 'cdn.example.com', 'i.pinimg.com'],
+    },
+  };
+  // Write all the Domain names from where you are getting all the images
+  export default nextConfig;
