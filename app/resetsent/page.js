4c5986357bad6161f1e37da229a1f9c0f587c17b
Aaron-1303 <111566868+Aaron-1303@users.noreply.github.com>
2024-05-27 09:46:28 -0600

    Non tech change
    regarding spelling mistake

--- app/resetsent/page.js
+++ app/resetsent/page.js
@@ -17,7 +17,7 @@ const AfterResetPassword = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
         <h1 className="text-white text-2xl mb-5">Thank you! an email was sent to your email address.</h1>
-        <h1 className="text-white text-2xl mb-5">Please check your email for futur instruction</h1>
+        <h1 className="text-white text-2xl mb-5">Please check your email for future instruction</h1>
         <button 
           onClick={handleClick}
           className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
