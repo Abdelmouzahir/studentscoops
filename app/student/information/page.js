113900e215d1ab4123a89234c2f062d482d54ab7
Abdelmouzahir <jalil.mouzahir@gmail.com>
2024-05-30 13:42:26 -0600

    text issue color

--- app/student/information/page.js
+++ app/student/information/page.js
@@ -64,7 +64,7 @@ const PersonalInfo = () => {
   return (
     <div className="min-h-screen py-40" style={{ backgroundImage: 'linear-gradient(115deg, #dfc42f, #faf7df)' }}>
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-xl overflow-hidden">
           <div
             className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
             style={{ backgroundImage: 'url(/assets/images/food.png)' }}
@@ -87,7 +87,7 @@ const PersonalInfo = () => {
             </div>
           </div>
           <div className="w-full lg:w-1/2 py-16 px-12">
-            <h2 className="text-3xl mb-4">Personal Information</h2>
+            <h2 className="text-3xl mb-4 text-black">Personal Information</h2>
             <form>
               <div className="grid grid-cols-2 gap-5">
                 <input 
@@ -145,7 +145,7 @@ const PersonalInfo = () => {
               {error && <div className="text-red-500 text-sm mb-4">{error}</div>}
               <div className="mt-5">
                 <input type="checkbox" className="border border-gray-400 rounded-md" />
-                <span className='ml-2'>
+                <span className='ml-2 text-black'>
                   I accept the <a href="#" className="text-yellow-500 font-semibold">Terms of Use</a> & <a href="#" className="text-yellow-500 font-semibold">Privacy Policy</a>
                 </span>
                 <button 
