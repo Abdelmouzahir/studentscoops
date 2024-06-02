19b93f11af187a6147f7b2726e87b6362606c48d
hunardeep720 <123598156+hunardeep720@users.noreply.github.com>
2024-06-02 12:58:05 -0600

    add modal

--- app/student/login.js
+++ app/student/login.js
@@ -1,10 +1,11 @@
 "use client";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { sendPasswordResetEmail } from "firebase/auth";
+import Modal from "@/Components/Modal";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -12,8 +13,9 @@ const Login = () => {
   const [loginError, setLoginError] = useState("");
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
 
-  const handleSignIn = (email) => {
+  const handleSignIn = () => {
     signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const user = userCredential.user;
@@ -26,10 +28,12 @@ const Login = () => {
       .catch((err) => {
         setLoginError("Invalid email or password");
         console.log(err);
+        console.log("Error signing in");
       });
   };
 
   const handleForgotPassword = () => {
+    setShowModal(false);
     sendPasswordResetEmail(auth, email)
       .then(() => {
         alert("Password reset email sent!");
@@ -44,67 +48,106 @@ const Login = () => {
       className="min-h-screen py-40"
       style={{ backgroundImage: "linear-gradient(115deg, #dfc42f, #faf7df)" }}
     >
-      <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-xl overflow-hidden">
-          <div
-            className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
-            style={{ backgroundImage: "url(/assets/images/login.jpeg)" }}
-          ></div>
-          <div className="w-full lg:w-1/2 py-16 px-12 text-black">
-            <h2 className="text-3xl mb-4 text-black">Login</h2>
-            <div className="mt-5">
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="border border-gray-400 py-1 px-2 w-full rounded-md"
-              />
-            </div>
-            <div className="mt-5">
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border border-gray-400 py-1 px-2 w-full rounded-md"
-              />
-            </div>
-            <div>
-              <p className="text-red-500 text-sm mb-4">{loginError}</p>
-            </div>
-            <div className="mt-5">
-              <p
-                className="text-yellow-500 cursor-pointer"
-                onClick={() => {
-                }}
-              >
-                Forget Password?
-              </p>
-            </div>
-            <div className="mt-4">
-              <button
-                onClick={handleSignIn}
-                className="w-full bg-yellow-500 py-3 text-center text-white mt-3 rounded-md"
-              >
-                Sign In
-              </button>
-            </div>
-
-            <div className="mt-3">
-              <span className="flex text-black">
-                <p>Don't have an account?</p>
-                <Link
-                  href="/student/register"
-                  className="text-yellow-500 font-semibold ml-2"
+      <Fragment>
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-xl overflow-hidden">
+            <div
+              className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
+              style={{ backgroundImage: "url(/assets/images/login.jpeg)" }}
+            ></div>
+            <div className="w-full lg:w-1/2 py-16 px-12 text-black">
+              <h2 className="text-3xl mb-4 text-black">Login</h2>
+              <div className="mt-5">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border border-gray-400 py-1 px-2 w-full rounded-md"
+                />
+              </div>
+              <div className="mt-5">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border border-gray-400 py-1 px-2 w-full rounded-md"
+                />
+              </div>
+              <div>
+                <p className="text-red-500 text-sm mb-4">{loginError}</p>
+              </div>
+              <div className="mt-5">
+                <p
+                  className="text-yellow-500 cursor-pointer"
+                  onClick={() => {
+                    setShowModal(true);
+                  }}
                 >
-                  Register
-                </Link>
-              </span>
+                  Forget Password?
+                </p>
+              </div>
+              <div className="mt-4">
+                <button
+                  onClick={handleSignIn}
+                  className="w-full bg-yellow-500 py-3 text-center text-white mt-3 rounded-md"
+                >
+                  Sign In
+                </button>
+              </div>
+
+              <div className="mt-3">
+                <span className="flex text-black">
+                  <p>Don't have an account?</p>
+                  <Link
+                    href="/student/register"
+                    className="text-yellow-500 font-semibold ml-2"
+                  >
+                    Register
+                  </Link>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <Modal
+          isVisible={showModal}
+          onClose={() => {
+            setShowModal(false);
+          }}
+        >
+          <div>
+            <p className="text-3xl mb-4 text-black">Reset Password</p>
+            <p className="py-3">
+              We will send you an email with instructions on how to reset your
+              password.
+            </p>
+            <div>
+              <form onSubmit={handleForgotPassword}>
+                <div>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="border border-gray-400 py-1 px-2 w-full rounded-md"
+                  />
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full bg-yellow-500 py-3 text-center text-white mt-3 rounded-md"
+                  >
+                    Email Me
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </Modal>
+      </Fragment>
     </div>
   );
 };
