820c1edfb1da75fbbd5e56d37a157c279bc9f2eb
Simrat Kaur <128754409+SimratKaur24@users.noreply.github.com>
2024-06-05 10:50:22 -0600

    Update footer.js
    In the Copyright section, I will add the dynamic year soon. But for now it is hard coded

--- app/main-component/layout/footer/footer.js
+++ app/main-component/layout/footer/footer.js
@@ -1,19 +1,66 @@
 import React from 'react'
-import Link from 'next/link'
-export default function Footer(){
-    return(
-        <footer className="w-full bg-gray-100 py-6 dark:bg-gray-800">
-        <div className="container flex items-center justify-between px-4 md:px-6">
-          <p className="text-sm text-gray-500 dark:text-gray-400">© 2024 Leftovers. All rights reserved.</p>
-          <nav className="flex gap-4">
-            <Link className="text-sm hover:underline" href="#">
-              Terms of Service
-            </Link>
-            <Link className="text-sm hover:underline" href="#">
-              Privacy Policy
-            </Link>
-          </nav>
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa6'
+
+const Footer = () => {
+  return (
+    <div className='pt-[5rem] pb-[3rem] bg-[#111111]'>
+      <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 
+          gap-[3rem] items-start pb-[2rem] border-b-2 border-white border-opacity-10'>
+        {/* footer section 1*/}
+        <div>
+          <h1 className='text-[24px] text-white mb-[1rem] font-bold uppercase'> GoJob </h1>
+          <p className=' text-[14px] text-white text-opacity-70' >
+            The world first and largest specialized talent solutions website
+            connects opportunities at great companies with highly skilled job seekers
+          </p>
+          {/* social media icons*/}
+          <div className='flex items-center space-x-3 mt-[1.5rem]'>
+            <div className='w-[2.4rem] h-[2.4rem] bg-blue-600 rounded-full flex items-center
+               justify-center flex-col'>
+              <FaFacebook className='text-white' />
+            </div>
+            <div className='w-[2.4rem] h-[2.4rem] bg-sky-400 rounded-full flex items-center
+               justify-center flex-col'>
+              <FaTwitter className='text-white' />
+            </div>
+            <div className='w-[2.4rem] h-[2.4rem] bg-red-600 rounded-full flex items-center
+               justify-center flex-col'>
+              <FaYoutube className='text-white' />
+            </div>
+            <div className='w-[2.4rem] h-[2.4rem] bg-red-400 rounded-full flex items-center
+               justify-center flex-col'>
+              <FaInstagram className='text-white' />
+            </div>
+          </div>
         </div>
-      </footer>
-    )
+        {/* 2nd part of footer*/}
+        <div>
+          <h1 className=' text-[22px] w-fit text-white font-semibold mb-[1.5rem]' >About US</h1>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Job</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Privacy</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Policy</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Application</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Candidates</p>
+        </div>
+        {/* 3nd part of footer*/}
+        <div>
+          <h1 className=' text-[22px] w-fit text-white font-semibold mb-[1.5rem]' >Quick Links</h1>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >All Jobs</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >How to Apply</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Job Fair</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Resume</p>
+        </div>
+        {/* 4th part of footer*/}
+        <div>
+          <h1 className=' text-[22px] w-fit text-white font-semibold mb-[1.5rem]' >Get In Touch </h1>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >+0123456789</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >example@eample.com</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-30 mb-[0.7rem] ' >Student Scoops</p>
+        </div>
+      </div>
+      <h1 className=' mt-[2rem] text-[14px] w-[80%] mx-auto text-white opacity-50'>COPYRIGHT BY STUDENT SCOOPS © 2024</h1>
+    </div>
+  )
 }
+
+export default Footer
