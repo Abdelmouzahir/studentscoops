975856e50d0cf0b8869a85ea4a1050c50d7b96cd
MoizInayat <moizinayat311@gmail.com>
2024-06-05 15:33:38 -0600

    reset password front end

--- app/auth/ResetPassword/page.js
+++ app/auth/ResetPassword/page.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
-export const page = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
+'use client';
+import { useState, useEffect } from 'react';
+import { auth } from '@/app/firebase/config';
+import { useRouter } from 'next/navigation';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import zxcvbn from 'zxcvbn';
+import { FaCheckCircle } from 'react-icons/fa'; 
+import { AiFillCloseCircle } from "react-icons/ai";
+import { BiSolidCommentError } from "react-icons/bi";
