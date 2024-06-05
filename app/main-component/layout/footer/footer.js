ee0566aca09fd717a5c92375923455b7da79ebdb
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-06-05 16:07:22 -0600

    small footer modification

--- app/main-component/layout/footer/footer.js
+++ app/main-component/layout/footer/footer.js
@@ -3,15 +3,14 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div className='pt-[5rem] pb-[3rem] bg-orange-400'>
+    <div className='pt-[5rem] pb-[3rem] bg-primary'>
       <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 
           gap-[3rem] items-start pb-[2rem] border-b-2 border-white border-opacity-10'>
         {/* footer section 1*/}
         <div>
           <h1 className='text-[24px] text-white mb-[1rem] font-bold uppercase'> StudentScoops </h1>
           <p className=' text-[14px] text-white text-opacity-70' >
-            The world first and largest specialized talent solutions website
-            connects opportunities at great companies with highly skilled job seekers
+        Initiative aims to address the challenges faced by students with low income by providing them access to discounted food and grocery items.
           </p>
           {/* social media icons*/}
           <div className='flex items-center space-x-3 mt-[1.5rem]'>
@@ -36,19 +35,17 @@ const Footer = () => {
         {/* 2nd part of footer*/}
         <div>
           <h1 className=' text-[22px] w-fit text-white font-semibold mb-[1.5rem]' >About US</h1>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Job</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >SAIT Club</p>
           <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Privacy</p>
           <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Policy</p>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Application</p>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Candidates</p>
+          
         </div>
         {/* 3nd part of footer*/}
         <div>
           <h1 className=' text-[22px] w-fit text-white font-semibold mb-[1.5rem]' >Quick Links</h1>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >All Jobs</p>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >How to Apply</p>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Job Fair</p>
-          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >Resume</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >SAIT Website</p>
+          <p className=' text-[15px] text-white w-fit hover:text-yellow-300 cursor-pointer text-opacity-70 mb-[0.7rem] ' >How to</p>
+          
         </div>
         {/* 4th part of footer*/}
         <div>
