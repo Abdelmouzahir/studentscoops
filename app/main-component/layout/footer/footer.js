a32da414dc2c3d6ce0a3a2efd549e8126cfdd1c9
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-06-05 16:08:36 -0600

    merge to master

--- app/main-component/layout/footer/footer.js
+++ app/main-component/layout/footer/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='pt-[5rem] pb-[3rem] bg-primary'>
       <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 
-          gap-[3rem] items-start pb-[2rem] border-b-2 border-white border-opacity-10'>
+          gap-[3rem] items-start pb-[2rem] border-b-2 border-white border-opacity-10'>
         {/* footer section 1*/}
         <div>
           <h1 className='text-[24px] text-white mb-[1rem] font-bold uppercase'> StudentScoops </h1>
@@ -15,19 +15,19 @@ const Footer = () => {
           {/* social media icons*/}
           <div className='flex items-center space-x-3 mt-[1.5rem]'>
             <div className='w-[2.4rem] h-[2.4rem] bg-blue-600 rounded-full flex items-center
-               justify-center flex-col'>
+              justify-center flex-col'>
               <FaFacebook className='text-white' />
             </div>
             <div className='w-[2.4rem] h-[2.4rem] bg-sky-400 rounded-full flex items-center
-               justify-center flex-col'>
+               justify-center flex-col'>
               <FaTwitter className='text-white' />
             </div>
             <div className='w-[2.4rem] h-[2.4rem] bg-red-600 rounded-full flex items-center
-               justify-center flex-col'>
+               justify-center flex-col'>
               <FaYoutube className='text-white' />
             </div>
             <div className='w-[2.4rem] h-[2.4rem] bg-red-400 rounded-full flex items-center
-               justify-center flex-col'>
+              justify-center flex-col'>
               <FaInstagram className='text-white' />
             </div>
           </div>
