a3ceaa75571ec2b3e4f51e0df6a2461560dcb9b3
MoizInayat <moizinayat311@gmail.com>
2024-06-05 15:35:47 -0600

    dispatch ResetPassword

--- app/auth/ResetPassword/page.js
+++ app/auth/ResetPassword/page.js
@@ -7,3 +7,27 @@ import zxcvbn from 'zxcvbn';
 import { FaCheckCircle } from 'react-icons/fa'; 
 import { AiFillCloseCircle } from "react-icons/ai";
 import { BiSolidCommentError } from "react-icons/bi";
+
+//backend part 
+
+
+
+
+
+
+//frontend part 
+const getPasswordStrengthBar = (score) => {
+  const strength = ['Very Weak', 'Weak', 'Fair', 'Good', 'Strong'];
+  const width = ['w-1/5', 'w-2/5', 'w-3/5', 'w-4/5', 'w-full'];
+  const colors = ['bg-red-500', 'bg-orange-500', 'bg-yellow-500', 'bg-green-500', 'bg-green-700'];
+  const textColor = ['text-red-500', 'text-orange-500', 'text-yellow-500', 'text-green-500', 'text-green-700'];
+  return (
+    <div className="mt-2 flex items-center">
+      <div className={`h-2 ${colors[score]} ${width[score]} rounded-md mr-2`}></div>
+      <p className={`text-sm ${textColor[score]}`}>{strength[score]}</p>
+    </div>
+  );
+};
+
+
+
