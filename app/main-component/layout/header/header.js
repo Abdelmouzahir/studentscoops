7d09adca085f614b5585d9ac942e4c9e10e1c1ac
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-06-07 13:10:20 -0600

    working on email confirmation after registration

--- app/main-component/layout/header/header.js
+++ app/main-component/layout/header/header.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Button } from '@/Components/ui/button';
+import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
 export default function Header() {
