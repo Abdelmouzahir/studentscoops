e35e0a26e0547b627f2c21ad1e232c7b511b2b4f
MoizInayat <123601592+MoizInayat@users.noreply.github.com>
2024-06-19 18:01:13 -0600

    personalize cover and img for restaurant

--- app/auth/loginRestaurant/page.js
+++ app/auth/loginRestaurant/page.js
@@ -61,7 +61,7 @@ const sign_in = () => {
     <div
       className="min-h-screen py-40"
       style={{ 
-        backgroundImage: "url(/assets/images/loginCover.jpg)",
+        backgroundImage: "url(/assets/images/restCover.jpg)",
         backgroundSize: "cover", // Adjusts the size of the background image
         backgroundPosition: "center", // Centers the background image
         backgroundRepeat: "no-repeat", // Prevents the background image from repeating
@@ -73,7 +73,7 @@ const sign_in = () => {
           <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-xl overflow-hidden">
             <div
               className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
-              style={{ backgroundImage: "url(/assets/images/saitLogin.jpg)" }}
+              style={{ backgroundImage: "url(/assets/images/restLogin.jpg)" }}
             ></div>
             <div className="w-full lg:w-1/2 py-16 px-12 text-black">
               <h2 className="text-3xl mb-4 text-black">Login</h2>
