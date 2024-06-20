2e289b047f8767971ec8ff9f748e7df784330fde
Aaron-1303 <111566868+Aaron-1303@users.noreply.github.com>
2024-06-20 08:11:11 -0600

    Update page.js Routing error

--- app/student/header_stud/page.js
+++ app/student/header_stud/page.js
@@ -6,7 +6,7 @@ export default function Header_stud({ handleSignOut }) {
   return (
     <header className="flex items-center justify-between h-16 px-4 md:px-6 bg-primary shadow-sm dark:bg-gray-950">
       <div className="flex items-center gap-4">
-        <Link href="/" className="flex items-center gap-2 text-2xl font-bold text-white" prefetch={false}>
+        <Link href="/student" className="flex items-center gap-2 text-2xl font-bold text-white" prefetch={false}>
           <span >STUDENT SCOOPS</span>
         </Link>
       </div>
