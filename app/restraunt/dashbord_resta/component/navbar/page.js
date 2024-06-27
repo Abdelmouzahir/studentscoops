778dc144976d0798d3c008d78499f6606d7003cf
sheraj <123598317+sheraj923@users.noreply.github.com>
2024-06-27 11:14:19 -0600

    done with the customer page of the restaurant

--- app/restraunt/dashbord_resta/component/navbar/page.js
+++ app/restraunt/dashbord_resta/component/navbar/page.js
@@ -46,7 +46,7 @@ export default function Navbar() {
               Orders
             </Link>
             <Link
-              href="#"
+              href="/restraunt/customer"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <Users className="h-4 w-4 text-primary" />
