ab88b99fbbb8d30c0b53dc7fdf0e6aa0b9a0e6cc
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-07-02 12:04:46 -0600

    console log

--- Components/UserGreeting.js
+++ Components/UserGreeting.js
@@ -3,6 +3,8 @@
 
 const UserGreeting = () => {
     const userDisplayName = sessionStorage.getItem("displayName") || "User";
+
+    console.log(userDisplayName)
   
     return (
       <h1 className="text-lg text-black-600 font-bold mt-2 ml-2">
