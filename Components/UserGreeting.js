7aba6e4823a320354058a903ef57e7ee31f35f13
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-07-03 19:34:54 -0600

    trying to fetch the name

--- Components/UserGreeting.js
+++ Components/UserGreeting.js
@@ -1,17 +1,34 @@
-'use client'
-// components/UserGreeting.js
+import { useEffect, useState } from "react";
 
 const UserGreeting = () => {
-    const userDisplayName = sessionStorage.getItem("displayName") || "User";
+  const [userName, setUserName] = useState("User");
+  const [email, setEmail] = useState("");
+  const [uid, setUid] = useState("");
 
-    console.log(userDisplayName)
-  
-    return (
-      <h1 className="text-lg text-black-600 font-bold mt-2 ml-2">
-        Hi! {userDisplayName}
-      </h1>
-    );
-  };
-  
-  export default UserGreeting;
-  
+  useEffect(() => {
+    // Ensure this runs only on the client-side
+    if (typeof window !== "undefined") {
+      const name = sessionStorage.getItem("name") || "User";
+      const userEmail = sessionStorage.getItem("email") || "";
+      const userUid = sessionStorage.getItem("uid") || "";
+      setUserName(name);
+      setEmail(userEmail);
+      setUid(userUid);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-lg text-black-600 font-bold mt-2 ml-2" >Welcome, {userName}!</h1>
+      <p>Email: {email}</p>
+      <p>UID: {uid}</p>
+    </div>
+  );
+};
+
+export default UserGreeting;
+
+
+
+
+
