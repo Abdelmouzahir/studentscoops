9b42337f3f7b181bffaefa25fe99937036d7bc2c
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-07-05 17:58:29 -0600

    add profil picture in saitadmin

--- Components/UserGreeting.js
+++ Components/UserGreeting.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { AiOutlineUser } from "react-icons/ai";
 
 const UserGreeting = () => {
   const [userName, setUserName] = useState("User");
   const [email, setEmail] = useState("");
   const [uid, setUid] = useState("");
+  const [userImage, setUserImage] = useState("");
 
   useEffect(() => {
     // Ensure this runs only on the client-side
@@ -11,24 +13,31 @@ const UserGreeting = () => {
       const name = sessionStorage.getItem("name") || "User";
       const userEmail = sessionStorage.getItem("email") || "";
       const userUid = sessionStorage.getItem("uid") || "";
+      const userImage = sessionStorage.getItem("imageUrl") || "";
+
       setUserName(name);
       setEmail(userEmail);
       setUid(userUid);
+      setUserImage(userImage);
     }
   }, []);
 
   return (
-    <div>
-      <h1 className="text-lg text-black-600 font-bold mt-2 ml-2" >Welcome, {userName}!</h1>
-     { /*<p>Email: {email}</p>*/}
-     { /*<p>UID: {uid}</p>*/}
+    <div className="inline-flex items-center ml-5 rounded-full">
+      <div className="bg-slate-200 relative h-12 w-12 border rounded-full overflow-hidden hover:bg-[#F29F3D]">
+        <img
+          className="w-full h-full rounded-full"
+          src={userImage}
+          alt="User Image"
+        />
+      </div>
+      <h1 className="text-lg text-black-600 font-bold mt-2 ml-2">
+        Welcome, {userName}!
+      </h1>
+      {/*<p>Email: {email}</p>*/}
+      {/*<p>UID: {uid}</p>*/}
     </div>
   );
 };
 
 export default UserGreeting;
-
-
-
-
-
--- app/sait-staff/page.js
+++ app/sait-staff/page.js
@@ -4,7 +4,6 @@ import SDashboard from './SDashboard';
 import RDashboard from './RDashboard';
 import Dash from "./overviewDash";
 import Settings from "./settingS";
-import { AiOutlineUser } from "react-icons/ai";
 import { LuLogOut } from "react-icons/lu";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/app/firebase/config';
@@ -45,11 +44,7 @@ export default function Page() {
       <SaitStaffNav isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} onTabClick={handleTabClick} />
       <div className={`flex-1 flex flex-col mx-auto transition-all duration-300 ease-in-out ${isCollapsed ? 'ml-20' : 'ml-64'}`}>
         <div className="flex justify-between items-center px-4 py-3 mt-2 mr-5">
-
-           <div className="inline-flex items-center ml-5 rounded-full">
-             <div className=" bg-slate-200 relative h-12 w-12 border rounded-full overflow-hidden hover:bg-[#F29F3D]">
-              <AiOutlineUser className="w-9 h-9 ml-1.5 mt-1 text-gray-600 " />
-             </div>    
+           <div className="inline-flex items-center ml-5 rounded-full">    
               <UserGreeting />
           </div>
           <button onClick={handeLogoutClick} className="inline-flex h-10 items-center justify-center rounded-md bg-gray-600 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#F29F3D] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#6c5ce7] disabled:pointer-events-none disabled:opacity-50 dark:bg-[#6c5ce7] dark:text-gray-50 dark:hover:bg-[#6c5ce7]/90 dark:focus-visible:ring-[#6c5ce7]">
