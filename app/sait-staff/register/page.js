e4cd69f0372340dc166caccb2d80cb6abdcdba35
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-07-16 11:57:08 -0600

    front end register pages

--- app/sait-staff/register/page.js
+++ app/sait-staff/register/page.js
@@ -178,7 +178,7 @@ const Register = () => {
     <div
       className="min-h-screen py-40"
       style={{ 
-        backgroundImage: "url(/assets/images/registerCover.jpg)",
+        backgroundImage: "url(/assets/images/salade.jpg)",
         backgroundSize: "cover", // Adjusts the size of the background image
         backgroundPosition: "center", // Centers the background image
         backgroundRepeat: "no-repeat", // Prevents the background image from repeating
@@ -188,7 +188,7 @@ const Register = () => {
         <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-xl overflow-hidden">
           <div
             className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
-            style={{ backgroundImage: "url(/assets/images/phone.png)" }}
+            style={{ backgroundImage: "url(/assets/images/Adminregister.jpg)" }}
           ></div>
           <div className="w-full lg:w-1/2 py-16 px-12 text-black">
             <h2 className="text-3xl mb-4 text-black">Register</h2>
