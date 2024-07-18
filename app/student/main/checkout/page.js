2c032ecde0d21e84f80a2555fdd576c455665d05
Aaron-1303 <111566868+Aaron-1303@users.noreply.github.com>
2024-07-18 11:04:08 -0600

    Deal of the day

--- app/student/main/checkout/page.js
+++ app/student/main/checkout/page.js
@@ -86,10 +86,7 @@ export default function Component() {
         {/* Card displaying restaurant avatar and address */}
         <Card className="p-6">
           <div className="flex items-center space-x-2">
-            <Avatar>
-              <AvatarImage src="/placeholder-user.jpg" />
-              <AvatarFallback>PC</AvatarFallback>
-            </Avatar>
+          <CreditCardIcon className="h-6 w-6 text-gray-500 dark:text-gray-400" />
             <div>
               <p className="font-semibold text-2xl">Payment</p>
               <div className='flex justify-center items-center'>
@@ -206,6 +203,29 @@ function HomeIcon(props) {
     </svg>
   );
 }
+
+
+function CreditCardIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="20" height="14" x="2" y="5" rx="2" />
+      <line x1="2" x2="22" y1="10" y2="10" />
+    </svg>
+  )
+}
+
+
 
 // Chevron Down Icon component
 function ChevronDownIcon(props) {
