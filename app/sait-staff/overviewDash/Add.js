122a329364bff93ee59bba48b27f3bfbff3ea653
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-07-25 11:28:13 -0600

    merge

--- app/sait-staff/overviewDash/Add.js
+++ app/sait-staff/overviewDash/Add.js
@@ -245,7 +245,6 @@ const Add = ({ admin, setAdmins, setIsAdding, fetchDataByUser }) => {
         </div>
         <div className="flex justify-end space-x-3">
           <button
-            onClick={handleClickAdd}
             type="submit"
             className="bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
